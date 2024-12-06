c9bc61f913584b2624b8c0c161a700a345dd2c73
Yang <ymj10003@gmail.com>
2024-12-06 23:40:31 +0900

    AI 최종 구현

--- app/src/main/java/com/example/myapplication/AIImageScreen.kt
+++ app/src/main/java/com/example/myapplication/AIImageScreen.kt
@@ -91,11 +91,18 @@ fun AIImageScreen(
     var SOHResponse by remember { mutableStateOf<SOHResponse?>(null) }
     var bitmap by remember { mutableStateOf<Bitmap?>(null) }
     var isLoading by remember { mutableStateOf(true) }
+    var SOH by remember { mutableStateOf<Int?>(0) }
 
     LaunchedEffect(Unit) {
         try {
             val SOHRequset = SOHRequest(device_number = "888777")
             SOHResponse = Retrofit.api.SOH(SOHRequset)
+
+            var temp =  ( SOHResponse?.predicted_SOH?.toFloatOrNull() )
+            if( temp != null ){
+                temp = temp * 100;
+                SOH = temp.toInt()
+            }
 
             SOHResponse?.let {
                 val imageResponse = Retrofit.api.Image(SOHRequset)
@@ -144,6 +151,15 @@ fun AIImageScreen(
                         .align(Alignment.TopStart)
                         .padding(16.dp)
                 ) {
+
+
+                    Text(
+                        text = "AI 배터리 충전 효율 분석 \n",
+                        color = Colors.Title,
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+
                     if (isLoading){
                         Text(
                             text = "예측 중입니다.",
@@ -155,7 +171,7 @@ fun AIImageScreen(
                     else {
                         SOHResponse?.let {
                             Text(
-                                text = it.predicted_SOH,
+                                text = "현재 배터리의 효율은 ${SOH}% 입니다. \n",
                                 color = Colors.Title,
                                 fontSize = 24.sp,
                                 fontWeight = FontWeight.Bold
@@ -163,12 +179,6 @@ fun AIImageScreen(
                         }
                     }
 
-                    Text(
-                        text = "AI 배터리 충전 효율 분석",
-                        color = Colors.Title,
-                        fontSize = 24.sp,
-                        fontWeight = FontWeight.Bold
-                    )
                     Text(
                         text = "• AI로 배터리의 충전 효율을 분석한 이미지입니다.",
                         color = Colors.Text,
@@ -184,6 +194,15 @@ fun AIImageScreen(
                     verticalArrangement = Arrangement.Center
                 ) {
                     Image(bitmap)
+
+
+                    Text(
+                        text = "현재까지의 데이터로 예측한 결과로 \n 실제 추세와는 다를 수 있습니다.",
+                        color = Colors.Text,
+                        fontSize = 16.sp,
+                        lineHeight = 35.sp,
+                        modifier = Modifier.padding(top = 8.dp)
+                    )
                 }
             }
         }
